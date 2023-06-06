78fe594b71e63056b4059beed4308184f956e6b1
Drissa <dcompte223@gmail.com>
2023-06-06 01:05:26 +0100

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,5 +1,10 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    name varchar(100)
+    id int primary key not null auto_increment,
+    name varchar(100),
+    date_of_birth date,
+    escape_attempts int(11),
+    neutered boolean,
+    weight_kg decimal
 );
